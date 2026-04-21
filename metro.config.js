dc66f955ef9f21f6bec4a55800affa4fe75177a5
divyanshsood <divyanshsood@icloud.com>
2026-04-22 01:49:52 +0530

    fix: simplify metro config

--- metro.config.js
+++ metro.config.js
@@ -5,13 +5,4 @@ const config = getDefaultConfig(__dirname);
 // Limit watch directories to reduce file watcher load
 config.watchFolders = ['src', 'app'];
 
-// Use fsevents directly instead of watchman on macOS
-config.server = {
-  ...config.server,
-  enhanceMiddleware: true,
-};
-
-// Disable watchman and use native file watching
-config.cacheVersion = '1.0.0';
-
 module.exports = config;
