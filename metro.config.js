ffc1c248e2a47f6102cb1d970007c7b29d2d9ff0
divyanshsood <divyanshsood@icloud.com>
2026-04-22 01:53:18 +0530

    fix: disable watchman to prevent EMFILE errors on macOS

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,7 @@ const config = getDefaultConfig(__dirname);
 // Limit watch directories to reduce file watcher load
 config.watchFolders = ['src', 'app'];
 
+// Disable watchman to avoid EMFILE errors on macOS
+config.watchman = false;
+
 module.exports = config;
