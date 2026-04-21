d4b21436dc065deef323d4a1c5a7d10493a97025
divyanshsood <divyanshsood@icloud.com>
2026-04-22 01:55:25 +0530

    chore: update metro config for watchman workaround

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,16 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Limit watch directories to reduce file watcher load
+// Limit watch directories
 config.watchFolders = ['src', 'app'];
 
-// Disable watchman to avoid EMFILE errors on macOS
+// Disable watchman
 config.watchman = false;
+
+// Configure resolver to reduce watched files
+config.resolver = {
+  ...config.resolver,
+  unstable_enablePackageExports: false,
+};
 
 module.exports = config;
