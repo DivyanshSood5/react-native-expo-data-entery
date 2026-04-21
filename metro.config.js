305df9d5db127fb5b065ff23aca00b6c8b344437
divyanshsood <divyanshsood@icloud.com>
2026-04-22 01:49:02 +0530

    chore: update metro config and start script for file watcher issues

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,13 @@ const config = getDefaultConfig(__dirname);
 // Limit watch directories to reduce file watcher load
 config.watchFolders = ['src', 'app'];
 
+// Use fsevents directly instead of watchman on macOS
+config.server = {
+  ...config.server,
+  enhanceMiddleware: true,
+};
+
+// Disable watchman and use native file watching
+config.cacheVersion = '1.0.0';
+
 module.exports = config;
